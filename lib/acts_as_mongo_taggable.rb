09f07e59e38adb249a936e827444c0ec0c0e0d90
Haris Amin <aminharis7@gmail.com>
2010-09-22 06:58:19 -0700

    Fixed ActiveSupport dependency for Rails 3 compatibility

--- lib/acts_as_mongo_taggable.rb
+++ lib/acts_as_mongo_taggable.rb
@@ -202,7 +202,6 @@ end
 %w{ models observers }.each do |dir|
   path = File.join(File.dirname(__FILE__), 'app', dir)
   $LOAD_PATH << path
-  ActiveSupport::Dependencies.load_paths << path
-  ActiveSupport::Dependencies.load_once_paths.delete(path)
+  ActiveSupport::Dependencies.autoload_paths << path
 end
 
